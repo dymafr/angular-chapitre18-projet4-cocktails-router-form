74764476b0aef17f09fc36fd2d5309327a5a3dd8
quentin <quentin@dyma.fr>
2024-12-20 12:41:47 +0100

    end lesson 03

--- src/app/views/cart/components/cart-ingredients-list.component.ts
+++ src/app/views/cart/components/cart-ingredients-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, input } from '@angular/core';
+import { Component, computed, effect, input } from '@angular/core';
 
 @Component({
   selector: 'app-cart-ingredients-list',
@@ -6,11 +6,29 @@ import { Component, effect, input } from '@angular/core';
   template: `
     <h2 class="mb-20">Liste des ingrédients</h2>
     <ul>
-      <!-- @for() -->
+      @for(ingredient of ingredientDisplays(); track $index) {
+      <li class="my-2">
+        {{ ingredient[0] }} : <strong>{{ ingredient[1] }}</strong>
+      </li>
+      } @empty {
+      <p>Aucun ingrédient n'a été ajouté pour le moment</p>
+      }
     </ul>
   `,
   styles: `:host { display: block; }`,
 })
 export class CartIngredientsListComponent {
-  ingredients = input<string[]>();
+  ingredients = input<string[]>([]);
+  ingredientDisplays = computed(() =>
+    Object.entries(
+      this.ingredients().reduce((acc, i) => {
+        if (acc[i]) {
+          acc[i]++;
+        } else {
+          acc[i] = 1;
+        }
+        return acc;
+      }, {} as { [s: string]: number })
+    )
+  );
 }
