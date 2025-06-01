2007dd4904db8d30f1738f9bc2dd6eab943fa273
Erwan Vallée <erwanp.vallee@gmail.com>
2025-06-01 12:37:15 +0200

    feat: maj v20 deps

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import {
   ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
 } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
+    provideHttpClient(), // ajoute HttpClient pour toute l’application pour utiliser httpResource
   ],
 };
